b91b8442403052d2ccb5b21ffc6cc7c1e5723fba
Maher Kefi <kefi.maher@esprit.tn>
2023-09-22 11:50:51 +0100

    11:50 22/09/2023

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import java.util.Date;
+
 public class Main {
 
     public static void main (String[] args){
-
+        Date d1 = new Date() ;
         Produit p1 = new Produit (1021," Lait "," Delice ");
         Produit p2 = new Produit (2510," Yayourt "," Vitalait ");
-        Produit p3 = new Produit (3250," Tomate "," Sicam ",1200);
+        Produit p3 = new Produit (3250," Tomate "," Sicam ",1200,d1);
         p1.affiche();
         p2.affiche();
         p3.affiche();
+
+      //  p1.toString() ;
 
     }
 }
--- src/Produit.java
+++ src/Produit.java
@@ -1,7 +1,10 @@
+import java.util.Date;
+
 public class Produit {
     int identifiant  ;
     String libelle , marque ;
     float prix ;
+    Date datexp ;
     public Produit() {}
     public Produit (int identifiant,String libelle,String marque) {
 
@@ -10,16 +13,17 @@ public class Produit {
         this.marque = marque  ;
 
     }
-    public Produit (int identifiant,String libelle,String marque,float prix ) {
+    public Produit (int identifiant,String libelle,String marque,float prix,Date da ) {
 
       this.identifiant = identifiant ;
       this.libelle = libelle ;
       this.marque = marque  ;
       this.prix = prix  ;
+      this.datexp = da ;
     }
     public  void affiche () {
         System.out.println("l'identfiant est :"+this.identifiant+" Libelle est :"+this.libelle
-                +" La maraque est :"+ this.marque+"la prix est :"+this.prix);
+                +" La maraque est :"+ this.marque+"la prix est :"+this.prix+"Date :"+this.datexp);
     }
 
     @Override
