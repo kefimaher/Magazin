ee244f8c3317a18e2e8f58d2a5c9e38e0d9b8fc5
Maher Kefi <kefi.maher@esprit.tn>
2023-09-22 11:39:33 +0100

    11:39 22/09/2023

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        Produit p1 = new Produit (1021," Lait "," Delice ");
+        Produit p2 = new Produit (2510," Yayourt "," Vitalait ");
+        Produit p3 = new Produit (3250," Tomate "," Sicam ",1200);
+        p1.affiche();
+        p2.affiche();
+        p3.affiche();
+
+    }
+}
--- src/Produit.java
+++ src/Produit.java
@@ -21,16 +21,7 @@ public class Produit {
         System.out.println("l'identfiant est :"+this.identifiant+" Libelle est :"+this.libelle
                 +" La maraque est :"+ this.marque+"la prix est :"+this.prix);
     }
-    public static void main (String[] args){
 
-        Produit p1 = new Produit (1021," Lait "," Delice ");
-        Produit p2 = new Produit (2510," Yayourt "," Vitalait ");
-        Produit p3 = new Produit (3250," Tomate "," Sicam ",1200);
-        p1.affiche();
-        p2.affiche();
-        p3.affiche();
-
-    }
 
 
 }
