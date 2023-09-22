57e9f145254f18d186e84eb6276f624a810b07f2
Maher Kefi <kefi.maher@esprit.tn>
2023-09-22 11:01:59 +0100

    22/09/2023 11:01

---
+++ src/Produit.java
@@ -0,0 +1,36 @@
+public class Produit {
+    int identifiant  ;
+    String libelle , marque ;
+    float prix ;
+    public Produit() {}
+    public Produit (int identifiant,String libelle,String marque) {
+
+        this.identifiant = identifiant ;
+        this.libelle = libelle ;
+        this.marque = marque  ;
+
+    }
+    public Produit (int identifiant,String libelle,String marque,float prix ) {
+
+      this.identifiant = identifiant ;
+      this.libelle = libelle ;
+      this.marque = marque  ;
+      this.prix = prix  ;
+    }
+    public  void affiche () {
+        System.out.println("l'identfiant est :"+this.identifiant+" Libelle est :"+this.libelle
+                +" La maraque est :"+ this.marque+"la prix est :"+this.prix);
+    }
+    public static void main (String[] args){
+
+        Produit p1 = new Produit (1021," Lait "," Delice ",0);
+        Produit p2 = new Produit (2510," Yayourt "," Vitalait ",0);
+        Produit p3 = new Produit (3250," Tomate "," Sicam ",1200);
+        p1.affiche();
+        p2.affiche();
+        p3.affiche();
+
+    }
+
+
+}
