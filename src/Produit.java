ff6e578ee77acb8b35c356efcdf6b696dea289ba
Maher Kefi <kefi.maher@esprit.tn>
2023-09-22 11:28:47 +0100

    11:28 22/09/2023

--- src/Produit.java
+++ src/Produit.java
@@ -23,8 +23,8 @@ public class Produit {
     }
     public static void main (String[] args){
 
-        Produit p1 = new Produit (1021," Lait "," Delice ",0);
-        Produit p2 = new Produit (2510," Yayourt "," Vitalait ",0);
+        Produit p1 = new Produit (1021," Lait "," Delice ");
+        Produit p2 = new Produit (2510," Yayourt "," Vitalait ");
         Produit p3 = new Produit (3250," Tomate "," Sicam ",1200);
         p1.affiche();
         p2.affiche();
